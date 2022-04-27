3b50acdfcc26710a4c94345fa37256d3c3232270
valdanosimao <valdanosimao@gmail.com>
2022-04-27 14:46:54 -0300

    implementação da camada de view

---
+++ src/LojaAppView.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+import br.com.vs.view.ProdutoCategoriaFrame;
+
+public class LojaAppView {
+
+	public static void main(String[] args) {
+		ProdutoCategoriaFrame produtoCategoriaFrame = new ProdutoCategoriaFrame();
+		produtoCategoriaFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
